0beb2500373c06debabe2ac3de1db41b49c3b41c
Derick Chen <jockonarock94@gmail.com>
2019-10-26 14:35:28 -0400

    Updated image analysis script integration with DB.

--- meme_sentiment.py
+++ meme_sentiment.py
@@ -1,6 +1,9 @@
 from g_ocr import detect_text
 from g_textentity import analyze_entities
 from g_textsentiment import analyze_sentiment
+
+from db_query import get_raw_by_id, insert_meme_processed
+
 import os
 import sys
 
@@ -9,8 +12,9 @@ CONFIG_FILE = "config/gcloud_credentials.json"
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CONFIG_FILE
 
 
-def gen_meme_sentiment(id, file_name, caption = None):
+def gen_meme_sentiment(id, file_name, date_string):
 
+	img = get_raw_by_id(id)
 	
 	img_text = detect_text(file_name)
 	
@@ -19,29 +23,33 @@ def gen_meme_sentiment(id, file_name, caption = None):
 
 	text_score, text_magnitude = analyze_sentiment(img_text)
 
-	if caption:
-		capt_score, capt_magnitude = analyze_sentiment(caption)
+	if img.caption:
+		capt_score, capt_magnitude = analyze_sentiment(img.caption)
 	else:
 		capt_score = None
 		capt_magnitude = None
 
 	print(f"entities: {entities_mentioned}\ntext_score = {text_score}, text_magnitude = {text_magnitude}\ncapt_score = {capt_score}, capt_magnitude = {capt_magnitude}")
 
+
 	data = {
 		"id":id,
-		"img_name": file_name,
-		"img_sentiment": -999,
-		"text_sentiment": text_score,
-		"text_magnitude": text_magnitude,
-		"capt_sentiment": capt_score,
-		"capt_magnitude": capt_magnitude,
+		"date": img.date,
+		"entities": " ".join(entities_mentioned),
+		"ocr_string": img_text,
+		"img_sentiment": -999, #guard value for unclassified meme
+		"text_sentiment": round(text_score,5),
+		"text_magnitude": round(text_magnitude,5),
+		"capt_sentiment": round(capt_score,5),
+		"capt_magnitude": round(capt_magnitude,5),
 
 	}
 
-	return data
+	status = insert_meme_processed(data)
+	print(status)
 
 
 if __name__ == "__main__":
 
-	ret = gen_meme_sentiment(sys.argv[1], sys.argv[2])
+	ret = gen_meme_sentiment(sys.argv[1], sys.argv[2], sys.argv[3])
 	print(ret)
