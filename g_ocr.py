4cefd1876231aecae344113d07a0293882550344
Derick Chen <jockonarock94@gmail.com>
2019-10-26 00:53:39 -0400

    Working Google vision API detect text script and credential validation.

---
+++ g_ocr.py
@@ -0,0 +1,29 @@
+CONFIG_FILE = "config/gcloud_credentials.json"
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    import os
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CONFIG_FILE
+
+
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
---
+++ test.py
@@ -0,0 +1,4 @@
+from g_ocr import detect_text
+
+
+detect_text("images/img1.jpg")
