6ae0ea9b016bd24eb1c6751b4b71dcd56afe73cb
Anand <anandc823@gmail.com>
2019-10-26 22:50:09 -0400

    created csv file

--- create_tagging_file.py
+++ create_tagging_file.py
@@ -11,6 +11,8 @@ print(df)
 for index,row in df.iterrows():
     gen_meme_sentiment(row['id'],"images/{}.jpg".format(row['id']),row['date'])
     #print(row['id'])
-tagged_df = pd.read_sql_query("select * from meme_processed;",conn)
-
+tagged_df = pd.read_sql_query("select id,comp_score from meme_processed;",conn)
+tagged_df['location'] = "//inlaid-rig-257102-vcm/meme_dataset/documents/images/"+tagged_df['id'].astype(str)+".jpg"
+tagged_df = tagged_df[['location','comp_score']]
+tagged_df.to_csv("scores.csv",index=False)
 print(tagged_df)
