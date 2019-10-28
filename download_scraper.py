1ecdf5256e0050a969424de15d0367ccff916a4b
Derick Chen <jockonarock94@gmail.com>
2019-10-27 20:01:06 -0400

    Additional meme scraper from Google images.

--- download_scraper.py
+++ download_scraper.py
@@ -1,20 +1,22 @@
-import praw
+
 import requests
 import re
 import os
-from bs4 import BeautifulSoup
+
 import sqlite3
-import shutil
+
 from db_query import insert_meme_raw
-from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
+
+from meme_sentiment import gen_meme_sentiment
 
 DB_LOCAL_PATH = "yhack.db"
 SQLALCHEMY_DATABASE_PATH = 'sqlite:///' + DB_LOCAL_PATH
 
 
 def main():
-    for f in os.listdir("downloads/jet_blue_memes"):
+    for f in os.listdir("jetblue_memes"):
         if ".jpg" in f:
             insert_data = {
                 'id': f.split(".")[0],
@@ -22,8 +24,11 @@ def main():
                 'caption': "",
                 'url':""
             }
-            print(insert_data)
-            print(insert_meme_raw(insert_data))
+            #print(insert_data)
+            #print(insert_meme_raw(insert_data))
+            gen_meme_sentiment(insert_data['id'], "jetblue_memes/"+f, "None")
+
+
 if __name__ == "__main__":
     main()
 
